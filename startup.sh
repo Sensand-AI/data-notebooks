3c04eed7141d6d602616a87d403dfbc3c3ac3f1d
Nav Rao <sirmews@gmail.com>
2024-02-02 05:29:16 +0000

    flask api tweaks

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ pip install -e .
 # Start the Flask app in the background
 # Assuming your Flask app is located at /workspaces/data-notebooks/api/app.py
 cd /workspaces/data-notebooks/api
-flask run --host=0.0.0.0 &
+flask run --host=0.0.0.0 --reload &
 
 # Start Jupyter Notebook server
 jupyter notebook --NotebookApp.token='' --NotebookApp.password='' --notebook-dir=/workspaces/data-notebooks/notebooks --port=8888
