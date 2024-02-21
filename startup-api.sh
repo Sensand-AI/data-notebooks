1986cde6b5ab009076aea620c03f10a7c83ccfc6
Nav Rao <sirmews@gmail.com>
2024-02-21 00:12:54 +0000

    new gis_utils package

--- startup-api.sh
+++ startup-api.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Install the aws_utils package in editable mode
-cd /workspaces/data-notebooks/packages/aws_utils
-pip install -e .
+# Install custom packages in editable mode
+pip install -r /workspaces/data-notebooks/requirements-custom.txt
 
 # Start Jupyter Kernel Gateway
 jupyter kernelgateway --ip=0.0.0.0 --port=8888 --KernelGatewayApp.allow_origin='*' --KernelGatewayApp.api='kernel_gateway.notebook_http' --KernelGatewayApp.seed_uri=${SEED_URI}
