83eba354873c2ba97e7709df82247ac055855973
Nav Rao <sirmews@gmail.com>
2024-02-23 04:53:41 +0000

    clean up redundant files

--- startup-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install custom packages in editable mode
-pip install -r /workspaces/data-notebooks/requirements-custom.txt
-
-# Start Jupyter Kernel Gateway
-jupyter kernelgateway --ip=0.0.0.0 --port=8888 --KernelGatewayApp.allow_origin='*' --KernelGatewayApp.api='kernel_gateway.notebook_http' --KernelGatewayApp.seed_uri=${SEED_URI}
