ccbbc9776b453189121edb4ff652147ab300b8cb
Jenna <jenna.guffogg@sensand.com>
2024-02-27 11:29:44 +1100

    test

--- startup-api.sh
+++ startup-api.sh
@@ -7,4 +7,4 @@ pip install -e .
 # Start Jupyter Kernel Gateway
 jupyter kernelgateway --ip=0.0.0.0 --port=8888 --KernelGatewayApp.allow_origin='*' --KernelGatewayApp.api='kernel_gateway.notebook_http' --KernelGatewayApp.seed_uri=${SEED_URI}
 
-echo "test"
+echo "test removal"
