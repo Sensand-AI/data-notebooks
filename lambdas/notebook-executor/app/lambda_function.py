4259007d337b080a21e501ab7f2d36d79dd41a90
Nav Rao <sirmews@gmail.com>
2024-03-05 03:45:22 +0000

    validate json and add tracing

--- lambdas/notebook-executor/app/lambda_function.py
+++ lambdas/notebook-executor/app/lambda_function.py
@@ -2,11 +2,14 @@
 
 import datetime
 import os
-
+import json
+from typing import Any, Dict
 import boto3
 import papermill as pm
 from datadog import initialize, statsd
 from ddtrace import tracer
+
+from jsonschema import validate, ValidationError
 
 from papermill.exceptions import PapermillExecutionError
 from botocore.exceptions import BotoCoreError
@@ -15,6 +18,21 @@ initialize(statsd_host=os.environ.get('DATADOG_HOST'))
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 REGION_NAME = 'us-east-1'
+
+@tracer.wrap(name='validate_event', service='notebook-executor')
+def validate_event(event: Dict[str, Any], schema: Dict[str, Any]) -> None:
+    """Validates the incoming event against the provided schema."""
+    validate(instance=event, schema=schema)
+
+@tracer.wrap(name='load_schema', service='notebook-executor')
+def load_schema(notebook_name: str):
+    """Load the JSON Schema for validating the notebook parameters."""
+    schema_path = os.path.join('/var/task/notebooks', notebook_name, 'schema.json')
+    if os.path.exists(schema_path):
+        with open(schema_path, 'r', encoding='utf-8') as schema_file:
+            return json.load(schema_file)
+    else:
+        raise FileNotFoundError(f"Schema file for notebook {notebook_name} not found.")
 
 @tracer.wrap(name='lambda_handler', service='notebook-executor')
 def lambda_handler(event, _):
@@ -34,6 +52,40 @@ def lambda_handler(event, _):
 
     # Extract notebook name and parameters from the event
     notebook_name = event.get('notebook_name')
+
+    if not notebook_name:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': 'Missing "notebook_name" in the event.'
+        }
+
+    # Load the JSON Schema for the specified notebook
+    try:
+        schema = load_schema(notebook_name)
+    except FileNotFoundError as e:
+        return {
+            'statusCode': 404,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': str(e)
+        }
+
+    # Validate the incoming event against the schema
+    try:
+        validate_event(event, schema)
+    except ValidationError as e:
+        return {
+            'statusCode': 400, 
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({'error': str(e)})
+        }
+
     parameters = event.get('parameters', {})
     save_output = event.get('save_output', True)
     s3_bucket = 'jenna-remote-sensing-sandbox' # S3 bucket to save the output notebook
@@ -52,8 +104,10 @@ def lambda_handler(event, _):
     )
 
     # Define the source and output notebook paths
-    input_path = os.path.join('/var/task/notebooks', notebook_name)
-    output_path = '/tmp/executed_notebookzz.ipynb'
+    # We store our notebooks in the lambda as `notebooks/notebook_name/notebook_name.ipynb`
+    notebook_file = notebook_name + '.ipynb'
+    input_path = os.path.join('/var/task/notebooks', notebook_name, notebook_file)
+    output_path = f'/tmp/executed_{notebook_name}_{datetime_stamp}.ipynb'
 
     # Check if the notebook exists
     if not os.path.exists(input_path):
@@ -75,11 +129,17 @@ def lambda_handler(event, _):
             statsd.increment('notebook.execution.success')
             return {
                 'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
                 'body': f'Notebook "{notebook_name}" executed successfully!'
             }
         except (PapermillExecutionError, BotoCoreError) as e:
             statsd.increment('notebook.execution.error')
             return {
                 'statusCode': 500,
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
                 'body': f'Error executing notebook "{notebook_name}": {str(e)}'
             }
