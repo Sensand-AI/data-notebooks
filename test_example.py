4bb27ceae16df83e83802d0af1de81f8165f6905
Josh Costa <127472742+CoshuaJ@users.noreply.github.com>
2024-05-21 04:25:15 +0000

    test import gis_utils

--- test_example.py
+++ test_example.py
@@ -1,5 +1,5 @@
 import pytest
-
+from gis_utils.colormap import create_levels
 
 def test_example_pass():
     x = 1
@@ -7,5 +7,8 @@ def test_example_pass():
 
 def test_example_fail():
     y = 2
-    # with pytest.raises(AssertionError):
-    assert y == 1
+    with pytest.raises(AssertionError):
+        assert y == 1
+
+levels = create_levels()
+print(levels)
