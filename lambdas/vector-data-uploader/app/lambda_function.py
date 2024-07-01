f0c5633d24399e8202bfda3a18a693591b00dcff
Tulsi Sapkota <tolsee3@gmail.com>
2024-07-01 16:16:52 +1000

    feat: write to db

--- lambdas/vector-data-uploader/app/lambda_function.py
+++ lambdas/vector-data-uploader/app/lambda_function.py
@@ -3,8 +3,15 @@
 import logging
 import os
 import shutil
+import botocore
+import signal
+import json
+import sys
+from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+from sqlalchemy import create_engine
 
-from app.lib.geopandas import find_shapefile, get_gdf
+
+from app.lib.geopandas import find_shapefile, get_gdf, write_to_postgis
 from app.lib.s3 import download_from_prefix
 from app.lib.config import Config
 
@@ -15,10 +22,52 @@ from ddtrace import tracer
 aws_default_region = os.getenv("AWS_DEFAULT_REGION")
 AWS_LAMBDA_FUNCTION_NAME = "vector-data-uploader"
 
+is_dev = os.environ.get("IS_DEV", "False") == "True"
+
+ENGINE = None
+
+def close_db():
+    global ENGINE
+    if ENGINE is not None:
+        logger.info('gracefully disconnecting db')
+        ENGINE.dispose()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, lambda: close_db(ENGINE))
+
+client = botocore.session.get_session().create_client('secretsmanager')
+cache_config = SecretCacheConfig()
+cache = SecretCache( config = cache_config, client = client)
+
+def init_db():
+    global ENGINE
+
+    if is_dev:
+        dbname=os.environ.get("POSTGRES_ENGINE", "")
+        user=os.environ.get("POSTGRES_USER", "")
+        password=os.environ.get("POSTGRES_PASSWORD", "")
+        host=os.environ.get("POSTGRES_HOST", "")
+        port=os.environ.get("POSTGRES_PORT", "")
+    else:
+        secret = cache.get_secret_string(os.environ.get("VECTOR_DATA_UPLOADER_DB_CREDENTIALS_SECRET_NAME", ""))
+        secret_dict = json.loads(secret)
+
+        dbname=secret_dict.get('dbname')
+        user=secret_dict.get('username')
+        password=secret_dict.get('password')
+        host=os.environ.get("VECOTOR_DATA_UPLOADER_ENGINE_PROXY_ENDPOINT", "")
+        port=secret_dict.get('port', 5432)
+
+
+    if ENGINE is None:
+        logger.info(f"Connecting to db {dbname} as {user} on {host}:{port}")
+        ENGINE = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{dbname}")
+
+# Configure logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger("VectorDataUploader")
 logger.setLevel(logging.INFO)
 
-@tracer.wrap()
 def delete_directory(directory_path):
     """
     Deletes the specified directory along with all its contents.
@@ -46,8 +95,7 @@ def delete_directory(directory_path):
             extra=dict(data={"directory": directory_path, "error": str(e)}),
         )
 
-
-@tracer.wrap()
+@tracer.wrap(name='lambda_handler')
 def lambda_handler(event, _):
     """
     Handles a Lambda event.
@@ -64,8 +112,16 @@ def lambda_handler(event, _):
     """
 
     logger.info("Payload: received", extra=dict(data={"event": event}))
+    init_db()
     download_directory = download_from_prefix(Config.UPLOAD_BUCKET_PREFIX)
     shapefile = find_shapefile(download_directory)
-    print(shapefile)
+    if shapefile is None:
+        logger.error("Shapefile: not found", extra=dict(data={"directory": download_directory}))
+        delete_directory(download_directory)
+        return {"statusCode": 404, "body": json.dumps({"message": "Shapefile not found"})}
+
     gdf = get_gdf(shapefile)
     logger.info("GeoDataFrame: created", extra=dict(data={"gdf": gdf.head()}))
+    write_to_postgis(gdf, ENGINE)
+    logger.info("GeoDataFrame: written to DB")
+    delete_directory(download_directory)
--- lambdas/vector-data-uploader/app/lib/geopandas.py
+++ lambdas/vector-data-uploader/app/lib/geopandas.py
@@ -1,9 +1,17 @@
 import os
 import json
+import logging
 import geopandas as gpd
+from sqlalchemy import engine
+from sqlalchemy.dialects.postgresql import JSONB
 from pathlib import Path
 
-def find_shapefile(directory: str) -> str | None:
+TABLE_NAME = 'vector_data'
+
+logger = logging.getLogger("VectorDataUploader")
+logger.setLevel(logging.INFO)
+
+def find_shapefile(directory: Path) -> str | None:
     """
     Finds the shapefile in the specified directory.
 
@@ -13,7 +21,7 @@ def find_shapefile(directory: str) -> str | None:
     Returns:
     - str: The path to the shapefile.
     """
-    print(f"Searching for shapefile in {directory}")
+    logger.info(f"Searching for shapefile in {directory}")
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".shp"):
@@ -21,7 +29,7 @@ def find_shapefile(directory: str) -> str | None:
 
     return None
 
-def get_gdf(path: Path) -> gpd.GeoDataFrame:
+def get_gdf(path: str) -> gpd.GeoDataFrame:
     """
     Reads the shapefile at the specified path and returns a GeoDataFrame.
 
@@ -36,9 +44,21 @@ def get_gdf(path: Path) -> gpd.GeoDataFrame:
 
     gdf_copy = read_gdf.copy()
     gdf_copy.drop(columns=['geometry'], inplace=True)
-    transformed_gdf['metadata'] = gdf_copy.apply(lambda row: json.dumps(row.to_dict()), axis=1)
 
+    transformed_gdf['metadata'] = gdf_copy.apply(lambda row: json.dumps(row.to_dict()), axis=1)
     transformed_gdf.set_geometry(read_gdf['geometry'], inplace=True)
 
     return transformed_gdf
+
+def write_to_postgis(gdf: gpd.GeoDataFrame, engine: engine.Engine):
+    """
+    Writes the GeoDataFrame to a PostGIS table.
+
+    Parameters:
+    - gdf (gpd.GeoDataFrame): The GeoDataFrame to write to the table.
+    - table_name (str): The name of the table to write to.
+    - connection_string (str): The connection string to the PostGIS database.
+    """
+
+    gdf.to_postgis(TABLE_NAME, engine, if_exists="append", index=True, dtype={"metadata": JSONB})
 
