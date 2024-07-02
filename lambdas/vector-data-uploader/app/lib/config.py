a28332e7f8f5cfc58c11ef4f00c9233da4dd7cb6
Tulsi Sapkota <tolsee3@gmail.com>
2024-07-02 16:40:24 +1000

    feat: add prefix for testing multiple cases

--- lambdas/vector-data-uploader/app/lib/config.py
+++ lambdas/vector-data-uploader/app/lib/config.py
@@ -1,8 +1,12 @@
 import os
 from pathlib import Path
 
+SUCCESS_PREFIX = 'shapefile-test/'
+DBF_MISSING_PREFIX = 'dbf-missing/'
+PRJ_MISSING_PREFIX = 'prj-missing/'
+
 class Config:
     UPLOAD_BUCKET = os.environ.get('VECTOR_DATA_UPLOADER_S3_BUCKET')
     # TODO: This should come from event later on
-    UPLOAD_BUCKET_PREFIX = 'invalid-shapefile/'
+    UPLOAD_BUCKET_PREFIX = DBF_MISSING_PREFIX
     TMP_DIR = Path('/tmp')
